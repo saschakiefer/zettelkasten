0e35bd5b139c5ce245f9ea778de18d8825725099
Sascha Kiefer <mail@sascha-kiefer.de>
2022-05-22 22:13:56 +0200

    Create Application Input Port

--- application/src/main/java/module-info.java
+++ application/src/main/java/module-info.java
@@ -3,4 +3,7 @@ module application {
     requires static lombok;
     exports com.saschakiefer.slipbox.application.ports.output;
     exports com.saschakiefer.slipbox.application.ports.input;
+    exports com.saschakiefer.slipbox.application.usecase;
+
+    provides com.saschakiefer.slipbox.application.usecase.CreateSlipNoteUseCase with com.saschakiefer.slipbox.application.ports.input.CreateSlipNoteInputPort;
 }
--- framework/src/main/java/module-info.java
+++ framework/src/main/java/module-info.java
@@ -5,4 +5,8 @@ module framework {
     requires org.apache.commons.io;
     requires static org.slf4j;
     requires static lombok;
+
+    provides com.saschakiefer.slipbox.application.ports.output.SlipNoteManagementOutputPort with com.saschakiefer.slipbox.framework.adapter.output.file.SlipNoteManagementFileAdapter;
+
+    uses com.saschakiefer.slipbox.application.usecase.CreateSlipNoteUseCase;
 }
