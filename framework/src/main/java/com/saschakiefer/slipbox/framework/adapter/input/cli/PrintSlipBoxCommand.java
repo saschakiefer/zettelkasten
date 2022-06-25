a8cf0fb5385c2ad8d281bb80cdfbf8341cb6d2b4
Sascha Kiefer <mail@sascha-kiefer.de>
2022-06-25 21:16:29 +0200

    Add Option to only print a specific note

--- framework/src/main/java/com/saschakiefer/slipbox/framework/adapter/input/cli/PrintSlipBoxCommand.java
+++ framework/src/main/java/com/saschakiefer/slipbox/framework/adapter/input/cli/PrintSlipBoxCommand.java
@@ -4,12 +4,14 @@ import com.saschakiefer.slipbox.application.ports.output.SlipNotePresenterOutput
 import com.saschakiefer.slipbox.application.usecase.VisualizeSlipBoxUseCase;
 import com.saschakiefer.slipbox.domain.entity.SlipNote;
 import com.saschakiefer.slipbox.domain.vo.SlipNoteId;
+import com.saschakiefer.slipbox.framework.adapter.output.file.SlipNoteFactory;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import picocli.CommandLine;
 
 import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
+import java.util.TreeMap;
 
 @Dependent
 @NoArgsConstructor
@@ -22,7 +24,7 @@ public class PrintSlipBoxCommand implements Runnable {
     @Inject
     SlipNotePresenterOutputPort slipNotePresenter;
 
-    @CommandLine.Parameters(index = "0", description = "Start note (default: 0 = all notes", defaultValue = "0")
+    @CommandLine.Parameters(index = "0", description = "Start note (default: 0 = all notes)", defaultValue = "0")
     String startNote;
 
     @CommandLine.Option(names = {"-r", "--root-only"}, description = "Only print root notes")
@@ -30,12 +32,26 @@ public class PrintSlipBoxCommand implements Runnable {
 
     @Override
     public void run() {
-        // TODO: Implement -r flag
-        SlipNote slipBox = SlipNote.builder()
-                .title("Slip Box")
-                .slipNoteId(new SlipNoteId("0"))
-                .children(visualizeSlipBoxUseCase.retrieveAllNotesAsTree())
-                .build();
+        SlipNote slipBox;
+
+        if (startNote == null || "0".equals(startNote))
+            slipBox = SlipNote.builder()
+                    .title("Slip Box")
+                    .slipNoteId(new SlipNoteId("0"))
+                    .children(visualizeSlipBoxUseCase.retrieveAllNotesAsTree())
+                    .build();
+        else {
+            SlipNote startNoteObject = SlipNoteFactory.creteFromFileById(new SlipNoteId(startNote));
+
+            TreeMap<SlipNoteId, SlipNote> startNoteAsMap = new TreeMap<>();
+            startNoteAsMap.put(startNoteObject.getSlipNoteId(), startNoteObject);
+
+            slipBox = SlipNote.builder()
+                    .title("Slip Box")
+                    .slipNoteId(new SlipNoteId("0"))
+                    .children(startNoteAsMap)
+                    .build();
+        }
 
         slipNotePresenter.present(slipBox, rootOnly);
     }
