cc0c451e6ad53549c57bf75503803bc0796147cc
Sascha Kiefer <mail@sascha-kiefer.de>
2022-07-11 20:12:22 +0200

    Update CLI Version

--- bootstrap/src/main/java/com/saschakiefer/slipbox/bootstrap/App.java
+++ bootstrap/src/main/java/com/saschakiefer/slipbox/bootstrap/App.java
@@ -17,7 +17,7 @@ import javax.inject.Inject;
 @CommandLine.Command(name = "zettelkasten",
         aliases = {"z"},
         mixinStandardHelpOptions = true,
-        version = "0.1.1",
+        version = "0.1.2",
         subcommands = {
                 CreateNewSlipNoteCommand.class,
                 ReassignSlipNoteCommand.class,
